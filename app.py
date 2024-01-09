7be39136ffc1caf2f5751064ff146fced9269a05
Harsha Vishwanadha <64513249+NoobPeen@users.noreply.github.com>
2024-01-09 08:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ st.sidebar.markdown('# Input Image')
 
 if st.button('Get random predictions'):
     response = requests.post(URI, data={})
-    # print(response.text)
     response = json.loads(response.text)
     preds = response.get('prediction')
     image = response.get('image')
@@ -23,7 +22,7 @@ if st.button('Get random predictions'):
     for layer, p in enumerate(preds):
         numbers = np.squeeze(np.array(p))
 
-        plt.figure(figsize=(32, 4))
+        fig, axes = plt.subplots(figsize=(32, 4))
 
         if layer == 2:
             row = 1
@@ -33,14 +32,15 @@ if st.button('Get random predictions'):
             col = 16
 
         for i, number in enumerate(numbers):
-            plt.subplot(row, col, i + 1)
-            plt.imshow((number * np.ones((8, 8, 3))).astype('float32'), cmap='binary')
-            plt.xticks([])
-            plt.yticks([])
+            ax = plt.subplot(row, col, i + 1)
+            ax.imshow((number * np.ones((8, 8, 3))).astype('float32'), cmap='binary')
+            ax.set_xticks([])
+            ax.set_yticks([])
             if layer == 2:
-                plt.xlabel(str(i), fontsize=40)
+                ax.set_xlabel(str(i), fontsize=40)
+        
         plt.subplots_adjust(wspace=0.05, hspace=0.05)
         plt.tight_layout()
 
-        st.text('Layer {}'.format(layer + 1), )
-        st.pyplot()
+        st.text('Layer {}'.format(layer + 1))
+        st.pyplot(fig)  # pass the figure object explicitly
